3922c37b0360b1f34ece5e886f2510595316d4e6
seydou soro <w.soro@sectester.io>
2023-01-27 10:35:32 +0000

    ExerciceXpGold

---
+++ src/app/share/share.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './button/button.component';
+import { ListComponent } from './list/list.component';
+
+
+
+@NgModule({
+  declarations: [
+    ButtonComponent,
+    ListComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    ButtonComponent,
+    ListComponent
+  ]
+})
+export class ShareModule { }
